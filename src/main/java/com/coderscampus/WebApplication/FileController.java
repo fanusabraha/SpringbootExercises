661d750c1e7668950a222ef8dfbd8eee73ba03d6
fanus abraha <fanus.abraha@yahoo.com>
2024-02-28 06:45:38 +0100

    Initial commit

---
+++ src/main/java/com/coderscampus/WebApplication/FileController.java
@@ -0,0 +1,28 @@
+package com.coderscampus.WebApplication;
+
+import com.coderscampus.WebApplicationService.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+// controller allows the independency injection
+@RestController
+public class FileController {
+    // Instantiating we dont need dat, Spring manages that through AutoWire
+    @Autowired
+    public FileService fileService;
+    @GetMapping("/read-lines")
+    public List<String> readLines () throws IOException {
+        return fileService.filestoRead("mein-info.txt");
+    }
+    @GetMapping ("/hello-world")
+    public String hello (){ return "hello";}
+    // retrieves the data when a pull request is made
+    @PostMapping("/hello-world")
+    public String hellopost (){ return "hello";}
+
+}
---
+++ src/main/java/com/coderscampus/WebApplicationService/FileService.java
@@ -0,0 +1,13 @@
+package com.coderscampus.WebApplicationService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+@Service
+public class FileService {
+    public List<String> filestoRead (String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName));
+    }
+}
